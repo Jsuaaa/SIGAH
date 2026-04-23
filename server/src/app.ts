82d7ecc4693aeffb33ad0b6e627392b93926db96
Jsuaaa <juanpablosuarezbrango@hotmail.com>
2026-04-23 18:52:23 -0500

    Infra config

--- server/src/app.ts
+++ server/src/app.ts
@@ -30,7 +30,7 @@ app.use(errorHandler);
 if (NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-  app.get('*', (_req, res) => {
+  app.get('/*splat', (_req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
