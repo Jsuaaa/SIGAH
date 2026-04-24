fc6a3079c6e02c8edbb12af31f801098dfefdce3
Jsuaaa <juanpablosuarezbrango@hotmail.com>
2026-04-24 17:15:43 -0500

    seed config in prisma

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -5,15 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.resolve(__dirname, 'prisma/schema.prisma'),
   datasource: {
     url: process.env.DATABASE_URL!,
   },
-  migrate: {
-    async seed() {
-      const { execSync } = await import('node:child_process');
-      execSync('npx tsx prisma/seed.ts', { stdio: 'inherit' });
-    },
+  migrations: {
+    seed: 'tsx prisma/seed.ts',
   },
 });
